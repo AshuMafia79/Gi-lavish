52904136c595f0eea64f2134f6ec623d29948db7
Lucifer09-m <yashpatade92@gmail.com>
2024-07-26 18:34:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7209709503:AAFKHP_qqg_X3mL8xvRuNEc-7gAHEz6Z0mw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Enjoyinlife")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1760223577")
 
 
 
