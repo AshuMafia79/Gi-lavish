180a976e45c525b12f592b99e38d49e2e7d0878c
Sudhansu42 <sudhansubhallavi42@gmail.com>
2024-08-07 05:15:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7064283772:AAGtllI7xWCkf1KfJ_fo9V0Y0eQHBJQ4o-w")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Sudhanshu_liftss_bot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5852474329")
 
 
 
