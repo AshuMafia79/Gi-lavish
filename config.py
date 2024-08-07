6e73e0ae252169ec87538999e10cae9fdde009ff
Sudhansuddosbot <sudhansuddos@gmail.com>
2024-08-07 19:07:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7224545035:AAFcVymgb-UyUwyKcATlv4N56aoRxluTkU4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Sudhanshu_liftss_ddos_bot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5852474329")
 
 
 
