44ac64224b2f7c393b386fc96b2d9d94e767819c
AshuMafia79 <sahurakhi1987@gmail.com>
2024-08-13 20:54:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7260571652:AAEPrUcWT8E45t7VvCyhTbLKEqQu7ZbEA50")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@MaFiA_GaMeRz_0p")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6576406220")
 
 
 
